13e4a18726be26dc201e9c5a25ef7113ece4a7db
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-10-07 07:49:28 +0530

    proxy added

--- server/server.js
+++ server/server.js
@@ -1,36 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const connectDB = require('./config/database');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const connectDB = require("./config/database");
 
-const authRoutes = require('./routes/auth');
-const serviceRoutes = require('./routes/services');
-const bookingRoutes = require('./routes/bookings');
-
+const authRoutes = require("./routes/auth");
+const serviceRoutes = require("./routes/services");
+const bookingRoutes = require("./routes/bookings");
 
 const app = express();
 
 // Connect to database
 connectDB();
- 
-
 
 // Middleware
-app.use(cors(
-    {
-        origin:"https://bikesaas-sk.vercel.app"   
-    }
-));
+const corsOptions = {
+  origin: "https://bikesaas-sk.vercel.app", // Your frontend URL
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/services', serviceRoutes);
-app.use('/api/bookings', bookingRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 const PORT = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
